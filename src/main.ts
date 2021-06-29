b237a78e5d38cc51c00a65cbb3965fab80b8f2d7
lelia <le1ia@me.com>
2021-06-28 20:13:51 -0400

    Using typeof test in place of null check

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function run(): Promise<void> {
 
   try {
     // Optionally enforce a whitelist of allowed repository licenses for forking
-    if (licenseWhitelist !== null) {
+    if (typeof licenseWhitelist !== 'undefined') {
       core.info(
         `⚖️ Checking repository license for ${repo} against provided whitelist...`
       )
