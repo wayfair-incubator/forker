914bd0f6d219cc7d4d54dd9b21345d501801f6bd
lelia <le1ia@me.com>
2021-11-11 18:14:45 -0500

    Remove unnecessary any annotation

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -5,7 +5,7 @@ test('action throws error without required inputs', async () => {
   delete process.env[`INPUT_REPO`]
   try {
     await run()
-  } catch (err: any) {
+  } catch (err) {
     expect(err).toEqual(new Error('Input required and not supplied: owner'))
   }
 })
