75c88172047b7838ae6ec1fd36a5baf0d82e303e
lelia <le1ia@me.com>
2022-09-07 02:23:52 -0400

    Adding fallback option for test tokens

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -4,6 +4,9 @@ import * as cp from 'child_process'
 import * as path from 'path'
 
 test('forker action runs with env inputs', () => {
+  process.env['INPUT_TOKEN'] = process.env['INPUT_TOKEN']
+    ? process.env['INPUT_TOKEN']
+    : process.env['ACCESS_TOKEN']
   process.env['INPUT_OWNER'] = 'tremor-rs'
   process.env['INPUT_REPO'] = 'tremor-bot'
   process.env['INPUT_ORG'] = 'wayfair-contribs'
